fa7e14095653440d87d97653524f70e6a8c77299
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-12 10:11:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
   final_results = data.each_with_object({}) do |(key, value), final_array|
+    binding.pry
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
